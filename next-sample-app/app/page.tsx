3ee5c200b4319381c8c82ccb4a9065c5972d09ce
Marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-06-04 18:16:04 +0500

    first commit

---
+++ next-sample-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="flex min-h-screen flex-col items-center justify-center p-29">
+      Hello World!!<br></br>
+      This is my first code that is written in NextJs Application.
+    </h1>
+  );
+}
+Home();
